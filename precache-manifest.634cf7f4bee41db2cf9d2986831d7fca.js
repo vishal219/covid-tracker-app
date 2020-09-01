aa028ed444a300dc4c8f0eed54e1d3bce94231fc
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-31 19:04:42 -0700

    Updates

--- precache-manifest.634cf7f4bee41db2cf9d2986831d7fca.js
+++ precache-manifest.634cf7f4bee41db2cf9d2986831d7fca.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f694ba6513990db7724cc476d6bda64",
+    "revision": "efa504cf555036bf1a6be42a8576f773",
     "url": "/covid-tracker-app/index.html"
   },
   {
-    "revision": "5dfe1681d9ca3232da7b",
+    "revision": "986c0704bff1cddac472",
     "url": "/covid-tracker-app/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "223ab3883fd04150ef22",
-    "url": "/covid-tracker-app/static/js/2.cb78aea5.chunk.js"
+    "revision": "854d5f21a69f4e41d42d",
+    "url": "/covid-tracker-app/static/js/2.6f10a352.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/covid-tracker-app/static/js/2.cb78aea5.chunk.js.LICENSE.txt"
+    "url": "/covid-tracker-app/static/js/2.6f10a352.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5dfe1681d9ca3232da7b",
-    "url": "/covid-tracker-app/static/js/main.2b33dd80.chunk.js"
+    "revision": "986c0704bff1cddac472",
+    "url": "/covid-tracker-app/static/js/main.b03613da.chunk.js"
   },
   {
     "revision": "dbc8184c75e044779a51",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-tracker-app/precache-manifest.b349e6c3a8db744b978121560a2775ea.js"
+  "/covid-tracker-app/precache-manifest.634cf7f4bee41db2cf9d2986831d7fca.js"
 );
 
 self.addEventListener('message', (event) => {
