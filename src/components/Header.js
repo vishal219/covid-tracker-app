f7a34cfe1b956016b4fdc76f4d32f4bf085c8bdc
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-31 17:46:43 -0700

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+
 import {makeStyles} from '@material-ui/core'
 const styles=makeStyles((theme)=>{
 
@@ -11,11 +11,11 @@ export default function Header() {
              <nav>
                 <div class="nav-wrapper">
                     <a href="#" class="brand-logo ">Covid-Info</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    {/* <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <Link to="/india">
                             India
                         </Link>
-                    </ul>
+                    </ul> */}
                 </div>
             </nav>
         
