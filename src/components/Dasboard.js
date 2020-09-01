bdf1d5c464d420de9c706b686137275501649dea
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-31 19:01:54 -0700

    commit

--- src/components/Dasboard.js
+++ src/components/Dasboard.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 import axios from  'axios'
 import pic from './cc.jpg'
+import {makeStyles} from '@material-ui/core'
+const style=makeStyles({
+  table:{
+    width: '100%',
+    display: 'table',
+    borderCollapse: 'collapse',
+    borderSpacing: '0',
+    boxSizing: 'inherit',
+    fontFamily: 'Segoe UI'
+  },
+  bold:{
+    textAlign:'left',
+
+  },
+  row:{
+    borderBottom: '1px solid lightgray',
+    padding: '3px 0px 3px 0px'
+  }
+})
 
 export default class Dasboard extends Component {
   state={
@@ -20,6 +39,7 @@ export default class Dasboard extends Component {
     }
     
     render() {
+      const classes=style()
        
         return (
           
@@ -27,13 +47,13 @@ export default class Dasboard extends Component {
                
                <img src={pic} alt="Covid-19" style={{width:'100%'}}></img>
                <h1 className="center">Covid-19 Stats</h1> 
-               <table>
+               <table className={classes.table}>
         <thead>
           <tr>
-              <th>Name</th>
-              <th>Total</th>
-              <th>Recovered</th>
-              <th>Deaths</th>
+              <th className={classes.bold}>Name</th>
+              <th className={classes.bold}>Total</th>
+              <th className={classes.bold}>Recovered</th>
+              <th className={classes.bold}>Deaths</th>
               
           </tr>
         </thead>
@@ -42,7 +62,7 @@ export default class Dasboard extends Component {
           {
             
               this.state.post.map((row)=>(
-              <tr key={row.Country}>
+              <tr key={row.Country} className={classes.row}>
                 <td>{row.Country}</td>
                 <td>{row.TotalConfirmed}  </td>
                 <td>{row.TotalRecovered} </td>
